95cf518987a7908c70b4b379b1ab56d2f6f25d1c
Mark Dickinson <monoposon@comcast.net>
2016-07-11 13:07:36 -0600

    made it about halfway, but then lunch ended

---
+++ README.md
@@ -0,0 +1,1 @@
+#Javascript functions part 1
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
-console.log("sanity check!");
+ // #1
+function sum(firstnum, secondnum) {
+  console.log(firstnum + secondnum);
+};
+sum(3, 5)
+
+ // #2
+ function isEqual(argument1, argument2) {
+  if (argument1 === argument2) {
+    console.log(true);
+  } else {
+    console.log(false);
+  };
+};
+
+ // #3
+ function discountPercentage(amount, discount) {
+  if (discount <= 0 || discount >= 100){
+    console.log("WARNING UNNACCEPTABLE DISCOUNT");
+  } else {
+    var decimal = discount / 100;
+    var total =  amount -(amount * decimal);
+    console.log("total: $" + total);
+  };
+};
+
+// #4
+function stringCapitalize(string) {
+  var stringArray = string.split(" ");
+  for 
+}
