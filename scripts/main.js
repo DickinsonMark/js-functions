42d549a6e4138bd84e5889c1dba370bab87cbf54
Mark Dickinson <monoposon@comcast.net>
2016-07-11 16:55:49 -0600

    finished

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,8 @@
 function sum(firstnum, secondnum) {
   console.log(firstnum + secondnum);
 };
-sum(3, 5)
+
+sum(3, 5); // 8
 
  // #2
  function isEqual(argument1, argument2) {
@@ -13,18 +14,23 @@ sum(3, 5)
   };
 };
 
+isEqual(1, 1) // true
+isEqual(1, 2) // false
+isEqual(1, "1") // false
+
  // #3
- function discountPercentage(amount, discount) {
+function discountPercentage(amount, discount) {
   if (discount <= 0 || discount >= 100){
     console.log("WARNING UNNACCEPTABLE DISCOUNT");
   } else {
     var decimal = discount / 100;
-    console.log("discount is: " amount * decimal);
-
+    console.log("discount is: $" + (amount * decimal));
     var total =  amount -(amount * decimal);
     console.log("total: $" + total);
-  };
+  }
 };
+
+discountPercentage(100, 15); //discount is: $15  total is: $85
 
 // #4
 function stringCapitalize(string) {
@@ -39,15 +45,52 @@ function stringCapitalize(string) {
   return result;
 };
 
-console.log(stringCapitalize("hello how are you?"));
+console.log(stringCapitalize("hello how are you?")); // "Hello How Are You?"
 
 // #5
 function evenNumbers(num) {
-  for (i = 0; i <= num; i++) {
-    if (i % 2 === 0) {
-      console.log(i);
+  if (num <= 100 && num >= 0){
+    for (i = 0; i <= num; i++) {
+      if (i % 2 === 0) {
+        console.log(i);
+      }
     }
+  } else {
+    console.log("need a number between 0 and 100!");
   }
 }
 
-evenNumbers(10);
+evenNumbers(10); // 0 2 4 6 8 10
+evenNumbers(10); // "need a number between 0 and 100!"
+
+// #6
+function isDivisible(num1, num2) {
+  if (num1 % num2 === 0) {
+    return true;
+  } else {
+    return false;
+  };
+};
+
+isDivisible(51, 8); // false
+isDivisible(36, 12); // true
+
+// #7 Bonus!
+function oddNumbers(numb){
+  if (numb < 40) {
+    for (i = 0; i <= 40; i++) {
+      if (i % 2 !== 0) {
+        console.log(i);
+      }
+    };
+  } else {
+    for (j = 40; j <= numb; j++) {
+      if (j % 2 !== 0){
+        console.log(j)
+      }
+    };
+  }
+};
+
+oddNumbers(38); // 1 3 5 ... 37 39
+oddNumbers(68); // 41 43 45 ... 65 67
